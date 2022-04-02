06ebe467a362f2a1422ebc32b41cc454252c9488
ClDaly2904 <c.l.daly@hotmail.com>
2022-04-02 17:11:58 +0000

    Create generateComputerChoice function to assign computer choice from a randomly generated number

--- assets/js/playscript.js
+++ assets/js/playscript.js
@@ -1,28 +1,40 @@
-  // Getting all the DOM elements needed for gameplay
-  const computerChoiceDisplay = document.getElementById("computer-choice");
-  const playerChoiceDisplay = document.getElementById("player-choice");
-  const resultMessageDisplay = document.getElementById("result-message");
-  const playerScore = document.getElementById("player-score");
-  const computerScore = document.getElementById("computer-score");
-  const rockButton = document.getElementById("rockButton");
-  const paperButton = document.getElementById("paperButton");
-  const scissorsButton = document.getElementById("scissorsButton");
+// Getting all the DOM elements needed for gameplay
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const playerChoiceDisplay = document.getElementById("player-choice");
+const resultMessageDisplay = document.getElementById("result-message");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const rockButton = document.getElementById("rockButton");
+const paperButton = document.getElementById("paperButton");
+const scissorsButton = document.getElementById("scissorsButton");
+
+/**
+ * Function to calculate computer's choice using random number generator between 0 and 2,
+ * assigns that number either rock, paper or scissors. Takes parameter from clickedButton function
+ */
+function generateComputerChoice() {
+  let choices = ['rockButton', 'paperButton', 'scissorsButton'];
+  let randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
+
+generateComputerChoice();
 
 /**
  * Creating function to recognise when player clicks on controls,
  * triggers game function and passes relevant parameter
  */
-  function clickedButton() {
+function clickedButton() {
   rockButton.addEventListener("click", function () {
-    gameplay("r");
+    gameplay("rockButton");
   })
   
-  paperButton.addEventListener("click", function () {
-    gameplay("p");
+paperButton.addEventListener("click", function () {
+  gameplay("paperButton");
   })
   
-  scissorsButton.addEventListener("click", function () {
-    gameplay("s");
+scissorsButton.addEventListener("click", function () {
+  gameplay("scissorsButton");
   })
 }
 
